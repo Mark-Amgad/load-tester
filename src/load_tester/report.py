caff6f0ce2f90bbefeca8318468bfd8887452ccf
Mark-Amgad <markamgad5@gmail.com>
2025-06-08 13:44:55 +0300

    feat: rm log

--- src/load_tester/report.py
+++ src/load_tester/report.py
@@ -8,7 +8,6 @@ from load_tester.models.http import HTTPResponseResult
 
 class Report:
     def __init__(self, results: List[HTTPResponseResult]):
-        print("Generating report...")
         self.results = results
         self.success_count = 0
         self.failure_count = 0
